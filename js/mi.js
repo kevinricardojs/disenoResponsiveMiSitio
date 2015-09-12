d91cd607d59bc7314ed8e638b90536d72d77a630
kevinricardojs <kevinricardojs@gmail.com>
2015-09-11 20:34:20 -0600

    Menu con js al 100%

--- js/mi.js
+++ js/mi.js
@@ -2,12 +2,14 @@ $(document).ready(function(){
 var i = 1
         $("header nav .boton").click(function(){
            if(i==1) {
-             $('ul').animate({left:'0'}); 
+             $('ul').animate({left:'-40%'});
+             $('.no-nav').animate({left:'60%'}) ;
              i = 0;
           }
            
           else {
            $('ul').animate({left:'-100%'});
+           $('.no-nav').animate({left:'0'}) ;
            i = 1;
                 }
             
